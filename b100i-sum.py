4c81625b4a6b32f7c66c605d64ceaed569ff5bad
Junming1120 <159005069+Junming1120@users.noreply.github.com>
2024-06-14 12:21:40 -0700

    1

--- b100i-sum.py
+++ b100i-sum.py
@@ -20,6 +20,6 @@ def sum(n1, d1, n2, d2):
 
 
 if __name__ == "__main__":
-    assert(1,3,2,1) == (7,3)
-    assert(1,4,3,4) == (1,1)
-    assert(5,2,3,5) == (31,10)
+    assert sum (1,3,2,1) == (7,3)
+    assert sum (1,4,3,4) == (1,1)
+    assert sum (5,2,3,5) == (31,10)
